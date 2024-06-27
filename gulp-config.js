81962e420da399aeb95d2b6308115551e917a63d
giuse <giuse@DESKTOP-C57ULJA>
2024-06-27 15:15:15 +0200

    Add project files.

---
+++ gulp-config.js
@@ -0,0 +1,10 @@
+module.exports = function () {
+    var config = {
+        webRoot: "C:\\inetpub\\wwwroot\\sccm.dev.local",
+        devRoot: "C:\\Users\\giuse\\source\\repos\\UniPaper-Websites",
+        solutionName: "UniPaper",
+        buildConfiguration: "Debug"
+    }
+
+    return config;
+}
